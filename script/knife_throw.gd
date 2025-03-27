5b628c75c90140f157e219b735faf87df991daec
MPpennings <123113@ashramcollege.nl>
2025-03-27 20:58:07 +0100

    aan het testen

--- script/knife_throw.gd
+++ script/knife_throw.gd
@@ -18,7 +18,7 @@ func _process(delta):
 			print("ray hit")
 			ray.get_collider().hit.rpc()
 		await get_tree().create_timer(1.0).timeout
-		queue_free.rpc()
+		queue_free()
 
 func _on_timer_timeout():
 	queue_free()
