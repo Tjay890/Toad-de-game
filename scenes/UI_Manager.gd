d5ea7b9ff5dc380e0607980fb8569b0d3cd13a68
Lil-Johny <123463@ashramcollege.nl>
2025-04-02 13:44:08 +0200

    portal af

--- scenes/UI_Manager.gd
+++ scenes/UI_Manager.gd
@@ -6,6 +6,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if Lobby.toad_id == multiplayer.get_unique_id():
+		return
 	player.posterUpdated.connect(UpdatePosterLabel)
 
 func UpdatePosterLabel(newValue:int):
