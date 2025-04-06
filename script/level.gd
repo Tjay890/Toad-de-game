5ecf7799d55bdbacd8b135ff9fce2291f8f829bf
MPpennings <123113@ashramcollege.nl>
2025-04-06 23:20:35 +0200

    push

--- script/level.gd
+++ script/level.gd
@@ -151,9 +151,12 @@ func player_is_winner():
 
 #@rpc("authority", "call_local", "reliable")
 func end_game():
-	print("escaped_players", len(escaped_players))
-	print("total_players -1 ", total_players -1)
-	if len(escaped_players) > 0:
+	if total_players - 1 <= 0: 
+		if Lobby.toad_id == 1:
+			toad_is_winner.rpc()
+		else:
+			player_is_winner.rpc()
+	elif len(escaped_players) > 0:
 		if (len(escaped_players) /(total_players - 1) ) >= 0.5:
 			player_is_winner.rpc()
 		else:
